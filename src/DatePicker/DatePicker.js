ad3f842b2138c541de9ea42c0ed8afdb7f7c794f
FOBABS <fobabs2win@rocketmail.com>
2020-03-28 23:13:51 +0100

    Create DatePicker component

--- src/DatePicker/DatePicker.js
+++ src/DatePicker/DatePicker.js
@@ -2,3 +2,13 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import './DatePicker.css';
 
+let oneDay = 60 * 60 * 24 * 1000;
+let todayTimestamp;
+
+class DatePicker extends Component {
+  state = {
+    getMonthDetails: []
+  }
+}
+
+export default DatePicker;

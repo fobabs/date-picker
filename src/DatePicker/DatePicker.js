7303ffbbc1a21525c6ec00f1bab14d98f5809117
FOBABS <fobabs2win@rocketmail.com>
2020-03-29 23:20:18 +0100

    Update DatePicker component

--- src/DatePicker/DatePicker.js
+++ src/DatePicker/DatePicker.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './DatePicker.css';
 
 let oneDay = 60 * 60 * 24 * 1000;
-let todayTimestamp;
+let todayTimestamp = Date.now() - (Date.now() % oneDay) + (new Date().getTimezoneOffset() * 1000 * 60);
 
 class DatePicker extends Component {
   state = {
     getMonthDetails: []
+  }
+
+  componentDidMount() {
+    window.addEventListener('click', this.addBackDrop);
+  }
+
+  componentWillMount() {
+    window.removeEventListener('click', this.addBackDrop);
   }
 }
 
